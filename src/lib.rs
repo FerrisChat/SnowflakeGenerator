fdd3c334cda886d56a1b4678ab7a65986ba59429
hydrostaticcog <hydro@hydrostaticcog.me>
2021-08-19 20:59:02 -0500

    Revert "revert new epoch"
    This reverts commit 506015aa00c69d0c6d1a0189f351451cbd72e3ce. There was never a issue with the snowflake generator, it was entirely downstream libs in the server.

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,13 @@
 //!
 //! <https://github.com/twitter-archive/snowflake/tree/snowflake-2010>
 //!
-//! # Warnings
-//! * This crate will not generate unique identifiers before the start of the Ferris Epoch.
-//! Do not use it in production until 01/01/2022 00:00:00.0000+00:00.
-//!
 //! # Changes
 //! * unsigned 128 bit integers are used
 //! * atomic 16 bit counters are used to allow up to 65,536 IDs to be generated every millisecond
 //! * response time must be less than 5 microseconds
 //!
 //! # Format
-//! * Bits 0 to 63: milliseconds since the Ferris Epoch (01/01/2022 00:00:00.0000+00:00).
+//! * Bits 0 to 63: milliseconds since the Ferris Epoch (01/01/2020 00:00:00.0000+00:00).
 //! Range of around 600,000,000 years.
 //! * Bits 64 to 71: the type of model (i.e. user, channel, guild)
 //! * Bits 73 to 85: internal 16-bit atomic counter
@@ -33,16 +29,13 @@ use std::time::{SystemTime, UNIX_EPOCH};
 /// A internal atomic counter that helps guarantee snowflakes will be globally unique.
 static INTERNAL_COUNTER: AtomicU16 = AtomicU16::new(0);
 /// The start of the Ferris Epoch in milliseconds since the Unix Epoch
-pub const FERRIS_EPOCH: u128 = 1_640_995_200_000;
+pub const FERRIS_EPOCH: u128 = 1_577_836_800_000;
 
 #[cfg(feature = "time-safety-checks")]
 /// A static variable to store the timestamp of the last snowflake generated.
 static mut LAST_TIME_CREATED: u128 = 0;
 
 /// Generates a snowflake from the current API version, the model type, and the node ID.
-///
-/// Snowflakes are guaranteed (almost: see footnotes) to be globally unique,
-/// even if they were generated in the same millisecond.
 ///
 /// # Panics
 /// Panics if the current time is behind the Unix Epoch.
@@ -52,10 +45,6 @@ static mut LAST_TIME_CREATED: u128 = 0;
 /// use ferrischat_snowflake_generator::generate_snowflake;
 /// assert_ne!(generate_snowflake::<0>(0, 0), generate_snowflake::<0>(0, 0));
 /// ```
-///
-/// # Footnotes
-/// Read the crate docs, and note that snowflakes are not unique before
-/// 01/01/2022 00:00:00.0000+00:00. Do not use this lib in production until then.
 #[inline]
 pub fn generate_snowflake<const API_VERSION: u8>(model_type: u8, node_id: u16) -> u128 {
     #[cfg(feature = "time-safety-checks")]
@@ -114,23 +103,15 @@ mod tests {
     fn unequal_snowflakes() {
         let snowflake_1 = generate_snowflake::<0>(0, u16::MAX);
         let snowflake_2 = generate_snowflake::<0>(0, u16::MAX);
-        println!("{:b}, {:b}", &snowflake_1, snowflake_2);
+        println!("{}, {}", snowflake_1, snowflake_2);
+        println!("{:b}, {:b}",snowflake_1, snowflake_2);
         assert_ne!(snowflake_1, snowflake_2);
     }
 
     #[test]
     fn all_unequal_snowflakes() {
-        // this code would panic until the current Ferris time reaches 0
-        // since before that date this is designed exclusively for testing
-        // and never to be used in production
-        // to solve that we have a different way of doing things
-        let max = if get_epoch_time() == 0 {
-            u16::MAX as usize
-        } else {
-            1_000_000
-        };
         let mut seen = HashSet::with_capacity(1_000_000);
-        for _ in 1..max {
+        for _ in 1..1_000_000 {
             let sf = generate_snowflake::<0>(0, 0);
             assert!(
                 seen.insert(sf),
